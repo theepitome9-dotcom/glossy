f9d4fbf02debda8232a7b0f2506e4cd6c3924994
theepitome9-dotcom <theepitome9@gmail.com>
2026-02-18 00:27:57 +0000

    Remove Vibecode Metro integration to fix EAS build

--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,9 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withVibecodeMetro } = require("@vibecodeapp/sdk/metro");
 
 const config = getDefaultConfig(__dirname);
 
 // Disable Watchman for Windows
 config.resolver.useWatchman = false;
-
-// SVG + Vibecode support
-withVibecodeMetro(config);
 
 config.transformer = {
   ...config.transformer,
